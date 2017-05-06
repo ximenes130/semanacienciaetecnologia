b4a9f37cf1382678fc26faa72057f22e58650039
ximenes <ximenes130@gmail.com>
2017-05-06 12:13:21 -0300

    Atualizando as paradas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 Vue.use(VueRouter);
 Vue.use(Vuetify);
 
+
+// Routes
+
 const app = httpVueLoader('./vue/app.vue');
 const notfound = httpVueLoader('./vue/404.vue');
 const index = httpVueLoader('./vue/index.vue');
 const local = httpVueLoader('./vue/local.vue');
 const programacao = httpVueLoader('./vue/programacao.vue');
+
+
+// Custom components
+
+const about = httpVueLoader('./vue/sections/about.vue');
+//   const subscription = '<div>subscription</div>';
+//   const schedule = '<div>schedule</div>';
+//   const campus-map = '<div>bcampus-map lahhh</div>';
+//   const campus-addres = '<div>campus-addres</div>';
+//   const fotos = '<div>fotos</div>';
+//   const contact = '<div>contact</div>';
+
+// Code
 
 const routes = [
 	{path: '*', component: notfound},
@@ -13,6 +29,10 @@ const routes = [
 	{path: '/local', component: local, name: 'Como chegar'},
 	{path: '/programacao', component: programacao, name: 'Programação'}
 ];
+
+// Registro de componentes
+
+Vue.component('about', about);
 
 var vueApp = new Vue({
 	el: '#app',
--- vue/index.vue
+++ vue/index.vue
@@ -1,21 +1,5 @@
 <template>
 	<div>
-		<v-container>
-			<v-row>
-				<v-col xs12 xl10 offset-xl1>
-					<v-carousel>
-						<v-carousel-item src="img/carrousel/1.jpg"></v-carousel-item>
-						<v-carousel-item src="img/carrousel/2.jpg"></v-carousel-item>
-						<v-carousel-item src="img/carrousel/3.jpg"></v-carousel-item>
-					</v-carousel>
-					<br>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae posuere sem. Maecenas ut lobortis felis. Ut rutrum condimentum turpis, a consectetur risus hendrerit et. In at libero a quam pretium auctor. Nullam sed lacus tristique turpis bibendum volutpat nec a ante. Quisque libero sapien, molestie sed aliquet nec, tristique eu augue. Integer a semper tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis ultrices dapibus ligula, at ultricies nisl suscipit id. Donec fringilla sem eu libero commodo faucibus eu eu arcu.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae posuere sem. Maecenas ut lobortis felis. Ut rutrum condimentum turpis, a consectetur risus hendrerit et. In at libero a quam pretium auctor. Nullam sed lacus tristique turpis bibendum volutpat nec a ante. Quisque libero sapien, molestie sed aliquet nec, tristique eu augue. Integer a semper tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis ultrices dapibus ligula, at ultricies nisl suscipit id. Donec fringilla sem eu libero commodo faucibus eu eu arcu.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae posuere sem. Maecenas ut lobortis felis. Ut rutrum condimentum turpis, a consectetur risus hendrerit et. In at libero a quam pretium auctor. Nullam sed lacus tristique turpis bibendum volutpat nec a ante. Quisque libero sapien, molestie sed aliquet nec, tristique eu augue. Integer a semper tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis ultrices dapibus ligula, at ultricies nisl suscipit id. Donec fringilla sem eu libero commodo faucibus eu eu arcu.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae posuere sem. Maecenas ut lobortis felis. Ut rutrum condimentum turpis, a consectetur risus hendrerit et. In at libero a quam pretium auctor. Nullam sed lacus tristique turpis bibendum volutpat nec a ante. Quisque libero sapien, molestie sed aliquet nec, tristique eu augue. Integer a semper tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis ultrices dapibus ligula, at ultricies nisl suscipit id. Donec fringilla sem eu libero commodo faucibus eu eu arcu.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae posuere sem. Maecenas ut lobortis felis. Ut rutrum condimentum turpis, a consectetur risus hendrerit et. In at libero a quam pretium auctor. Nullam sed lacus tristique turpis bibendum volutpat nec a ante. Quisque libero sapien, molestie sed aliquet nec, tristique eu augue. Integer a semper tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis ultrices dapibus ligula, at ultricies nisl suscipit id. Donec fringilla sem eu libero commodo faucibus eu eu arcu.</p>
-				</v-col>
-			</v-row>
-		</v-container>
+		<about></about>
 	</div>
 </template>
