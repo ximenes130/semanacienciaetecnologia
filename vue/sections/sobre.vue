8b3e61eb6a942cc2ea991bf8b749ba0c7799e3bd
ximenes <ximenes130@gmail.com>
2017-05-08 21:47:39 -0300

    Adicionando efeito de parallax a imagem da logo

--- vue/sections/sobre.vue
+++ vue/sections/sobre.vue
@@ -6,7 +6,7 @@
 					<span class="white--text">Sobre o evento</span>
 				</v-card-title>
 			</v-card-row>
-			<v-card-row img="img/logo.png" height="300px" class="grey darken-4"></v-card-row>
+			<v-parallax src="img/logo.png" height="400" class="grey darken-4"></v-parallax>
 			<v-card-text class="blue-grey darken-3 white--text">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae posuere sem. Maecenas ut lobortis felis. Ut rutrum condimentum turpis, a consectetur risus hendrerit et.</p>
 			</v-card-text>
