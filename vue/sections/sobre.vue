fffd7f94abb9ac71cffd1154e0e1e7143561ee72
ximenes <ximenes130@gmail.com>
2017-05-11 21:46:05 -0300

    Adicionando efeito de parallax na seção sobre

--- vue/sections/sobre.vue
+++ vue/sections/sobre.vue
@@ -6,11 +6,13 @@
 					<span class="white--text">Sobre o evento</span>
 				</v-card-title>
 			</v-card-row>
-			<v-card-row img="img/sobre-bg.jpg">
+			<img src="img/logo.svg" class="logo">
+			<v-parallax src="img/sobre-bg.jpg" height="263"> </v-parallax>
+		<!--	<v-card-row img="img/sobre-bg.jpg">
 				<v-spacer></v-spacer>
 				<img src="img/logo.svg" class="logo">
 				<v-spacer></v-spacer>
-			</v-card-row>
+			</v-card-row> -->
 			<v-card-text class="teal darken-3 white--text">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae posuere sem. Maecenas ut lobortis felis. Ut rutrum condimentum turpis, a consectetur risus hendrerit et.</p>
 			</v-card-text>
@@ -20,6 +22,8 @@
 
 <style scoped>
 	.logo{
+		position: absolute;
+		z-index: 2;
 		width: 500px;
 		margin: 20px;
 		max-width: calc(100% - 40px);
