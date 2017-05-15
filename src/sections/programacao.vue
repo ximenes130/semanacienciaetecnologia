0d357e53d46ffb9d5b7b7a61b6871817d378a139
Samuel Novaes Sena <samuel.novaes.96@gmail.com>
2017-05-14 21:18:11 -0300

    Incorporando agenda do Google Calendar

--- src/sections/programacao.vue
+++ src/sections/programacao.vue
@@ -1,92 +1,52 @@
 <template>
 	<div>
 		<v-card horizontal class="mt-4">
-			<v-card-column>
-				<v-card-row actions class="teal darken-2">
-					<v-card-title>
-						<span class="white--text">Programação</span>
-						<v-spacer></v-spacer>
-						<v-dialog v-model="modal" class="hidden-md-and-up">
-							<v-btn slot="activator" flat class="white--text">
-								<v-icon>event</v-icon> {{cdata}}
-							</v-btn>
-							<v-date-picker v-model="data" :days="days" :months="months" :date-format="format" :no-title="true"></v-date-picker>
-						</v-dialog>
-					</v-card-title>
+			<v-card-column class="amber darken-1 white--text">
+				<v-card-row height="100px" class="teal white--text">
+					<v-card-text>
+						<strong>Programação</strong>
+						<div>Veja o cronograma das palestras e oficinas do evento</div>
+					</v-card-text>
 				</v-card-row>
-				<v-card-row class="teal darken-4 white--text">
-					<v-card-row class="hidden-sm-and-down">
-						<v-date-picker v-model="data" :days="days" :months="months" :date-format="format" :no-title="true"></v-date-picker>
-					</v-card-row>
-					<v-card-row height="290px" style="overflow: auto">
-						<v-card-text>
-							<v-list three-line>
-								<v-list-item v-for="(evento, index) in ccronograma" :key="index">
-									<v-list-tile>
-										<v-list-tile-content class="white--text">
-											<v-list-tile-title>
-												{{evento.horario}} - {{evento.titulo}}
-											</v-list-tile-title>
-											<v-list-tile-sub-title>
-												{{evento.descricao}}
-											</v-list-tile-sub-title>
-										</v-list-tile-content>
-									</v-list-tile>
-									<v-divider light></v-divider>
-								</v-list-item>
-							</v-list>
-						</v-card-text>
-					</v-card-row>
+				<v-card-row actions class="teal darken-2">
+					<v-spacer></v-spacer>
+					<v-dialog  fullscreen transition="v-dialog-bottom-transition" :overlay="false" v-model="dcalendar">
+						<v-btn flat class="white--text" slot="activator">
+							<v-icon left>schedule</v-icon>Ver programação
+						</v-btn>
+						<v-card>
+							<v-card-row>
+								<v-toolbar>
+									<v-btn icon="icon" @click.native="dcalendar = false">
+										<v-icon>close</v-icon>
+									</v-btn>
+									<v-toolbar-title>Programação</v-toolbar-title>
+								</v-toolbar>
+								<iframe id="iframe-calendar" src="https://calendar.google.com/calendar/embed?showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=WEEK&height=600&wkst=1&hl=pt_BR&bgcolor=%23339999&src=2mo6dkn8pco11p1hlf95534kso%40group.calendar.google.com&color=%23853104&ctz=America%2FSao_Paulo&dates=20170612/20170613" scrolling="no"></iframe>
+							</v-card-row>
+						</v-card>
+					</v-dialog>
 				</v-card-row>
 			</v-card-column>
+			<v-card-row img="img/inscreva-se.jpg" height="175px"></v-card-row>
 		</v-card>
 	</div>
 </template>
 
 <script>
-import cronograma from '../js/cronograma.js'
 export default {
 	data(){
 		return {
-			data: new Date("2017-8-10"),
-			days: [
-				"Domingo",
-				"Segunda",
-				"Terça",
-				"Quarta",
-				"Quinta",
-				"Sexta",
-				"Sábado"
-			],
-			months: [
-				"Janeiro",
-				"Fevereiro",
-				"Março",
-				"Abril",
-				"Maio",
-				"Junho",
-				"Julho",
-				"Agosto",
-				"Setembro",
-				"Outubro",
-				"Novembro",
-				"Dezembro"
-			],
-			format(val){
-				return new Date(val);
-			},
-			cronograma,
-			modal: false
-		}
-	},
-	computed: {
-		cdata(){
-			let d = new Date(this.data);
-			return `${d.getDate()}/${d.getMonth()+1}`;
-		},
-		ccronograma(){
-			return this.cronograma[this.cdata];
+			dcalendar: false
 		}
 	}
-}
+};
 </script>
+
+<style>
+#iframe-calendar {
+	width: 100%;
+	height: calc(100vh - 56px);
+	border: none;
+}
+</style>
