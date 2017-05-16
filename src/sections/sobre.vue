288511bf2ed6b3ea36e4cfe2e6807bd569488bce
ximenes <ximenes130@gmail.com>
2017-05-16 10:38:13 -0300

    Atualizando texto da secção sobre

--- src/sections/sobre.vue
+++ src/sections/sobre.vue
@@ -10,8 +10,12 @@
 				<img src="img/logo.svg" class="logo">
 			</v-parallax>
 			<v-card-text class="teal darken-3 white--text">
-				<p>A Semana Acadêmica dos cursos de Computação do Instituto Federal de Educação, Ciência e Tecnologia de Brasília - IFB <strong>(Campus Taguatinga)</strong>, é um evento institucional promovido pela comissão organizadora do Evento e pelo Centro Acadêmico para Computação. A Semana da Computação é um evento acadêmico, sem fins lucrativos, realizado anualmente que visa reunir estudantes, docentes e a comunidade em geral em torno de atividades científicas e culturais. A Semana da Computação acontecerá no Instituto Federal de Brasília - IFB Campus Taguatinga nos dias 12 e 13 de junho de 2017. O evento é uma oportunidade para participar de palestras e minicursos com temas relevantes na área da Computação e áreas afins.</p>
-				<p>Em sua terceira edição, a programação conta com palestras que ocorrerão no Auditório do Campus, minicursos que ocorrem em laboratórios especificos, entre outras atividades. Confira na programação maiores informações sobre o evento.</p>
+				<p><strong>Nos dias 12 e 13 de junho</strong>, o  Instituto Federal de
+					Brasília (IFB) <em>campus</em> Taguatinga sediará a I Semana de Ciência e
+					Tecnologia (SC&T), onde estudantes, professores e pesquisadores
+					discutem ideias sobres diversos temas da Automação,  Física e
+					Computação. Para isso, o evento reunirá palestras, oficinas, workshops,
+					rodas de conversas,	exposições, protótipos de produtos e muito mais.</p>
 			</v-card-text>
 		</v-card>
 	</div>
