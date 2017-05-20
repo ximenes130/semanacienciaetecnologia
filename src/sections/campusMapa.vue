bb118f2f265415e4a973035ed0e68507a9d35df7
ximenes <ximenes130@gmail.com>
2017-05-19 23:37:03 -0300

    Adicionando 'Expansion panel' na secção mapa

--- src/sections/campusMapa.vue
+++ src/sections/campusMapa.vue
@@ -7,44 +7,26 @@
 				</v-card-title>
 			</v-card-row>
 			<v-card-row>
+				<v-spacer ></v-spacer>
+				<v-expansion-panel>
+					<v-expansion-panel-content class="indigo white--text" v-for="(mapa,index) in mapas" :key="index">
+						<div slot="header" class="white--text"> {{mapa.titulo}}</div>
+						<v-card>
+							<img :src="mapa.imagem"  class="mapa-img">
+						</v-card>
+					</v-expansion-panel-content>
+				</v-expansion-panel>
 				<v-spacer></v-spacer>
-				<img src="img/cards/planta.png" class="mapa-img">
-				<v-spacer></v-spacer>
-			</v-card-row>
-			<v-card-row class="indigo">
-				<v-card-title>
-					<span class="white--text">Legenda</span>
-				</v-card-title>
 			</v-card-row>
 			<v-card-text class="indigo darken-3 white--text">
 				<v-list>
-					<v-list-item>
+					<v-list-item v-for="(legenda,index) in legendas" :key="index">
 						<v-list-tile>
 							<v-list-tile-avatar>
-								<v-icon class="green--text">fiber_manual_record</v-icon>
+								<v-icon :class="legenda.cor + '--text'">fiber_manual_record</v-icon>
 							</v-list-tile-avatar>
 							<v-list-tile-content>
-								<v-list-tile-title class="white--text">Bla Bla Bla</v-list-tile-title>
-							</v-list-tile-content>
-						</v-list-tile>
-					</v-list-item>
-					<v-list-item>
-						<v-list-tile>
-							<v-list-tile-avatar>
-								<v-icon class="yellow--text">fiber_manual_record</v-icon>
-							</v-list-tile-avatar>
-							<v-list-tile-content>
-								<v-list-tile-title class="white--text">Bla Bla Bla</v-list-tile-title>
-							</v-list-tile-content>
-						</v-list-tile>
-					</v-list-item>
-					<v-list-item>
-						<v-list-tile>
-							<v-list-tile-avatar>
-								<v-icon class="red--text">fiber_manual_record</v-icon>
-							</v-list-tile-avatar>
-							<v-list-tile-content>
-								<v-list-tile-title class="white--text">Bla Bla Bla</v-list-tile-title>
+								<v-list-tile-title class="white--text">{{legenda.texto}}</v-list-tile-title>
 							</v-list-tile-content>
 						</v-list-tile>
 					</v-list-item>
@@ -55,7 +37,26 @@
 </template>
 
 <style scoped>
-	.mapa-img{
-		width: 100%;
-	}
+.mapa-img{
+	width: 100%;
+}
 </style>
+
+<script>
+export default {
+	data(){
+		return {
+			mapas: [
+				{titulo:'Mapa do campus',      		  imagem:'img/mapas/Mapa teste.png'},
+				{titulo:'Salas de aula - Terreo',          imagem:'img/mapas/Mapa teste.png'},
+				{titulo:'Salas de aula - 1º Andar',      imagem:'img/mapas/Mapa teste.png'}
+			],
+			legendas: [
+				{texto:'teste 1',				cor:'green'},
+				{texto:'teste 2',				cor:'yellow'},
+				{texto:'teste 3',				cor:'red'}
+			]
+		}
+	}
+};
+</script>
