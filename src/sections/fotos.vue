2306947a7074d4692b167f271055b5f0f6e6621e
Samuel Novaes Sena <samuel.novaes.96@gmail.com>
2017-05-22 04:22:33 -0300

    Detalhes

--- src/sections/fotos.vue
+++ src/sections/fotos.vue
@@ -16,11 +16,11 @@
 		data(){
 			return {
 				fotos: [
-				'img/carousel/1.jpg',
-				'img/carousel/2.jpg',
-				'img/carousel/3.jpg',
-				'img/carousel/4.jpg',
-				'img/carousel/5.png'
+					'img/carousel/1.jpg',
+					'img/carousel/2.jpg',
+					'img/carousel/3.jpg',
+					'img/carousel/4.jpg',
+					'img/carousel/5.png'
 				]
 			}
 		}
