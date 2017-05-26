702ed422af6422cd0b6947927c18bd4356edc2c8
ximenes <ximenes130@gmail.com>
2017-05-25 22:12:14 -0300

    Atualizando texto do cartão Inscrição de projetos

--- src/sections/inscricaoProjeto.vue
+++ src/sections/inscricaoProjeto.vue
@@ -4,13 +4,32 @@
 		<v-card-column>
 			<v-card-row class="indigo white--text">
 				<v-card-text>
-					<strong>Setor M-Norte QNM 40</strong>
-					<div>Taguatinga, Brasília - DF, 72146-050</div>
+					<strong class="title">Inscrição de Projetos</strong>
+					<div class="pt-1">
+						Estamos aceitando a submissão de projetos para serem apresentados
+						na forma de banners até o dia <strong>99/99/2017</strong>, devendo
+						seguir os seguintes requisitos:
+					</div>
+					<v-layout row-sm column child-flex-sm>
+						<v-card class="transparent elevation-0">
+							<div class="ml-1 mr-1">
+								<div  class="ma-1 pa-1 indigo darken-2">Só fazendo um teste, para testar essa parada.... Legal de mais da conta né</div>
+								<div  class="ma-1 pa-1 indigo darken-2">Blalbalblal abalblabl lab labl alblalbalb lablalbal alb alb lablabl ala blab alba</div>
+							</div>
+						</v-card>
+						<v-card class="transparent elevation-0">
+							<div class="ml-1 mr-1">
+								<div  class="ma-1 pa-1 indigo darken-2">Balblablalb lab lalb lbalblablbla lab  alb alblab labl alb lablalbalbalb ballalba</div>
+								<div  class="ma-1 pa-1 indigo darken-2">Balbal labl alblalbabl lalb alb lbalal alb lablalal albblalbblalb labllabl blab alb</div>
+							</div>
+						</v-card>
+					</v-layout>
+
 				</v-card-text>
 			</v-card-row>
 			<v-card-row actions class="indigo darken-2">
 				<v-btn flat class="white--text" @click.native="openUrl('https://www.google.com.br/maps/place/Instituto+Federal+de+Bras%C3%ADlia+Campus+Taguatinga/@-15.7936003,-48.1132566,14.58z/data=!4m5!3m4!1s0x0:0x63a91cbeaf63f89e!8m2!3d-15.7942254!4d-48.1021088');">
-					<v-icon left light>directions</v-icon> Ver no mapa
+					<v-icon left light>assignment</v-icon> Inscrever Projeto
 				</v-btn>
 			</v-card-row>
 		</v-card-column>
@@ -18,11 +37,11 @@
 </template>
 
 <script>
-	export default {
-		methods: {
-			openUrl(url){
-				window.open(url, '_blank');
-			}
+export default {
+	methods: {
+		openUrl(url){
+			window.open(url, '_blank');
 		}
 	}
+}
 </script>
