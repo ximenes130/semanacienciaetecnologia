f6791e54c4b92d5e9d458ac23974d451bafce0d2
ximenes <ximenes130@gmail.com>
2017-05-29 17:57:38 -0300

    Corrigindo texto

--- src/sections/inscricaoProjeto.vue
+++ src/sections/inscricaoProjeto.vue
@@ -13,24 +13,30 @@
 					<v-layout row-sm column child-flex-sm>
 						<v-card class="transparent elevation-0">
 							<div class="ml-1 mr-1">
-								<div  class="ma-1 pa-2 indigo darken-2">Só fazendo um teste, para testar essa parada.... Legal de mais da conta né</div>
-								<div  class="ma-1 pa-2 indigo darken-2">Blalbalblal abalblabl lab labl alblalbalb lablalbal alb alb lablabl ala blab alba</div>
+								<div  class="ma-1 pa-2 indigo darken-2">Área de conhecimento: Ciências Exatas e da Terra</div>
+								<div  class="ma-1 pa-2 indigo darken-2">Possuir protótipo ou banner impresso no momento de inscrição</div>
 							</div>
 						</v-card>
 						<v-card class="transparent elevation-0">
 							<div class="ml-1 mr-1">
-								<div  class="ma-1 pa-2 indigo darken-2">Balblablalb lab lalb lbalblablbla lab  alb alblab labl alb lablalbalbalb ballalba</div>
-								<div  class="ma-1 pa-2 indigo darken-2">Balbal labl alblalbabl lalb alb lbalal alb lablalal albblalbblalb labllabl blab alb</div>
+								<div  class="ma-1 pa-2 indigo darken-2">A pesquisa deverá estar em andamento ou finalizada</div>
+								<div  class="ma-1 indigo darken-2">
+									<div class="pa-2">Submeter resumo com no mínimo 100 e máximo de 200 palavras</div>
+								</div>
 							</div>
 						</v-card>
 					</v-layout>
-
 				</v-card-text>
 			</v-card-row>
 			<v-card-row actions class="indigo darken-2">
-				<v-btn flat class="white--text" @click.native="openUrl('https://www.google.com.br/maps/place/Instituto+Federal+de+Bras%C3%ADlia+Campus+Taguatinga/@-15.7936003,-48.1132566,14.58z/data=!4m5!3m4!1s0x0:0x63a91cbeaf63f89e!8m2!3d-15.7942254!4d-48.1021088');">
-					<v-icon left light>assignment</v-icon> Inscrever Projeto
-				</v-btn>
+				<v-layout row-sm column child-flex-sm class="ml-2 mr-2">
+					<v-btn flat class="white--text" @click.native="openUrl('http://www.sbpcnet.org.br/portoseguro/arquivos/modelo_resumo.doc');">
+						<v-icon left light>file_download</v-icon> Modelo de resumo
+					</v-btn>
+					<v-btn flat class="white--text" @click.native="openUrl('https://www.google.com.br/maps/place/Instituto+Federal+de+Bras%C3%ADlia+Campus+Taguatinga/@-15.7936003,-48.1132566,14.58z/data=!4m5!3m4!1s0x0:0x63a91cbeaf63f89e!8m2!3d-15.7942254!4d-48.1021088');">
+						<v-icon left light>assignment</v-icon> Inscrever Projeto
+					</v-btn>
+				</v-layout>
 			</v-card-row>
 		</v-card-column>
 	</v-card>
