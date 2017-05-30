67f58b80ed5e44a50d173cf826bf4f83b39a16a7
ximenes <ximenes130@gmail.com>
2017-05-29 22:39:24 -0300

    Alterando foto do carão inscricaoProjeto

--- src/sections/inscricaoProjeto.vue
+++ src/sections/inscricaoProjeto.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-card horizontal class="mt-4">
-		<v-card-row img="img/cards/mapa.png" height="175px"></v-card-row>
+			<v-card-row height="400px" img="/img/cards/projeto.jpg"></v-card-row>
 		<v-card-column>
 			<v-card-row class="indigo white--text">
 				<v-card-text>
