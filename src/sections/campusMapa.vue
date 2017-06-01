e160b0c5841d88df4287ffc900dfa0308f9948c5
ximenes <ximenes130@gmail.com>
2017-06-01 09:57:32 -0300

    Atualização de cor dos mapas

--- src/sections/campusMapa.vue
+++ src/sections/campusMapa.vue
@@ -10,7 +10,7 @@
 			<v-expansion-panel class="elevation-0">
 				<v-expansion-panel-content class="indigo white--text elevation-0" v-for="(mapa,index) in mapas" :key="index">
 					<div slot="header" class="white--text elevation-0"> {{mapa.titulo}}</div>
-					<v-card class="pt-2 pb-2">
+					<v-card class="pt-2 pb-2 indigo lighten-3">
 						<img :src="mapa.imagem"  class="mapa-img">
 					</v-card>
 				</v-expansion-panel-content>
