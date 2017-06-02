9a46d1d2326982a665bf19c2db190a37a63df536
Samuel Novaes <samuel@mint.Home>
2017-06-01 23:22:53 -0300

    Gerando release com zip

--- release.js
+++ release.js
@@ -1,25 +1,34 @@
 const fs = require('fs-extra');
 const path = require('path');
+const EasyZip = require('easy-zip').EasyZip;
+const zip = new EasyZip();
 
 function update(from, to){
-	fs.remove(to)
-	.then(()=>{
-		fs.copy(from, to)
-		.catch((err)=>{
-			throw err;
+	return new Promise((resolve, reject)=>{
+		fs.remove(to)
+		.then(()=>{
+			fs.copy(from, to)
+			.then(()=>{
+				resolve();
+			});
 		});
-	})
-	.catch((err)=>{
-		throw err;
 	});
+}
+
+async function updateAll(cb){
+	await update(path.join(__dirname, 'img'), path.join(__dirname, 'release', 'www', 'img'));
+	await update(path.join(__dirname, 'dist'), path.join(__dirname, 'release', 'www', 'dist'));
+	await update(path.join(__dirname, 'index.html'), path.join(__dirname, 'release', 'www', 'index.html'));
+	cb();
 }
 
 fs.ensureDir(path.join(__dirname, 'release', 'www'))
 .then(()=>{
-	update(path.join(__dirname, 'img'), path.join(__dirname, 'release', 'www', 'img'));
-	update(path.join(__dirname, 'dist'), path.join(__dirname, 'release', 'www', 'dist'));
-	update(path.join(__dirname, 'index.html'), path.join(__dirname, 'release', 'www', 'index.html'));
-})
-.catch((err)=>{
-	throw err;
+	updateAll(()=>{
+		zip.zipFolder(path.join(__dirname, 'release'), ()=>{
+			zip.writeToFile(path.join(__dirname, 'release.zip'), ()=>{
+				console.log('Release concluído!');
+			});
+		});
+	});
 });
