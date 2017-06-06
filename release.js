1cc6ff3c170e7afae7deb48584f81b0a477b2f83
Samuel Novaes <samuel.novaes.96@gmail.com>
2017-06-06 19:09:01 -0300

    Adaptando release.js para versões mais antigas do Node

--- release.js
+++ release.js
@@ -3,23 +3,24 @@ const path = require('path');
 const EasyZip = require('easy-zip').EasyZip;
 const zip = new EasyZip();
 
-function update(from, to){
-	return new Promise((resolve, reject)=>{
-		fs.remove(to)
+function update(from, to, cb){
+	fs.remove(to)
+	.then(()=>{
+		fs.copy(from, to)
 		.then(()=>{
-			fs.copy(from, to)
-			.then(()=>{
-				resolve();
-			});
+			cb();
 		});
 	});
 }
 
-async function updateAll(cb){
-	await update(path.join(__dirname, 'img'), path.join(__dirname, 'release', 'www', 'img'));
-	await update(path.join(__dirname, 'dist'), path.join(__dirname, 'release', 'www', 'dist'));
-	await update(path.join(__dirname, 'index.html'), path.join(__dirname, 'release', 'www', 'index.html'));
-	cb();
+function updateAll(cb){
+	update(path.join(__dirname, 'img'), path.join(__dirname, 'release', 'www', 'img'), ()=>{
+		update(path.join(__dirname, 'dist'), path.join(__dirname, 'release', 'www', 'dist'), ()=>{
+			update(path.join(__dirname, 'index.html'), path.join(__dirname, 'release', 'www', 'index.html'), ()=>{
+				cb();
+			});
+		});
+	});
 }
 
 fs.ensureDir(path.join(__dirname, 'release', 'www'))
