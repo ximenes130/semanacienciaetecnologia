42bd6dc3550a68d88308b9fcea75658e6f7cce02
Samuel Novaes <samuel@localhost.localdomain>
2017-06-09 01:45:58 -0300

    Organizando seções

--- src/index.vue
+++ src/index.vue
@@ -1,16 +1,16 @@
 <template>
 	<v-container fluid class="grey lighten-2">
 		<sobre id="sobre"></sobre>
+		<fotos id="fotos"></fotos>
 		<campus-endereco id="campus-endereco"></campus-endereco>
-		<inscricao id="inscricao"></inscricao>
-		<inscricao-minicurso id="inscricao"></inscricao-minicurso>
-		<inscricao-projeto id="inscricaoProjeto"></inscricao-projeto>
 		<campus-mapa id="campus-mapa"></campus-mapa>
 		<programacao id="programacao"></programacao>
 		<palestrantes id="palestrantes"></palestrantes>
-		<fotos id="fotos"></fotos>
-		<contato id="contato"></contato>
+		<inscricao id="inscricao"></inscricao>
+		<inscricao-minicurso id="inscricao-minicurso"></inscricao-minicurso>
+		<inscricao-projeto id="inscricao-projeto"></inscricao-projeto>
 		<organizacao id="organizacao"></organizacao>
+		<contato id="contato"></contato>
 	</v-container>
 </template>
 
